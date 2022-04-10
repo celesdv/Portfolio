f60bc9c0b618392466fa96429984374c4504d4aa
celesdv <66212987+celesdv@users.noreply.github.com>
2022-04-10 15:07:58 -0300

    portfolio: comienzo, banner, header, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent
+    SocialComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
